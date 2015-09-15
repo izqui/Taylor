7f6a18e68ddc6f50ec1ca1379f6418ef110626e7
Jorge Izquierdo <jorge@izqui.me>
2015-09-15 14:54:13 +0200

    Set configuration to Debug

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
 carthage checkout
-xcodebuild -workspace TaylorPlayground.xcworkspace/ -scheme "Universal Framework" -configuration Release
-xcodebuild -workspace TaylorPlayground.xcworkspace/ -scheme Taylor -configuration Release
+xcodebuild -workspace TaylorPlayground.xcworkspace/ -scheme "Universal Framework" -configuration Debug
+xcodebuild -workspace TaylorPlayground.xcworkspace/ -scheme Taylor -configuration Debug
+open TaylorPlayground.xcworkspace
