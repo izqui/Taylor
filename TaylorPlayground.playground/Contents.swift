7e2289527f4330e5d1c02f278f7c3f0bb6725cb1
Jorge Izquierdo <jorge@izqui.me>
2015-09-15 19:02:49 +0200

    Add should execute infinitely

--- TaylorPlayground.playground/Contents.swift
+++ TaylorPlayground.playground/Contents.swift
@@ -1,6 +1,10 @@
 //: Playground - noun: a place where people can play
 
 import Taylor
+import XCPlayground
+
+XCPSetExecutionShouldContinueIndefinitely()
+
 let server = Taylor.Server()
 
 server.get("/") {
