aa1b96a4df314b3f3dee837d06ff8887b99bc78f
Jorge Izquierdo <jorge@izqui.me>
2015-09-16 12:39:44 +0200

    Update carthage dependency

---
+++ Carthage/Checkouts/taylor/Taylor/stdext.swift
@@ -0,0 +1,14 @@
+import Foundation
+
+extension String {
+    /// Components of a path; ignores the leading slash and empty components
+    /// e.g. "/foo/bar/baz" -> [foo, bar, baz]
+    
+    var taylor_pathComponents: [String] {
+        return self.componentsSeparatedByString("/").filter { $0 != "" }
+    }
+    
+    var NS: NSString {
+        return self as NSString
+    }
+}
